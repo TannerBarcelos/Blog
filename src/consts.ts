bf21002942c4ba54da732630b3fc5c003a1d4b24
Tanner Barcelos <barcelos.m.tanner@gmail.com>
2023-07-13 23:02:25 -0700

    Initial updates + content'

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Tanner Barcelos'
+export const SITE_DESCRIPTION =
+  'Welcome to my blog! I write about web development, software engineering, big data, and more.'
